7e6978ccb98470b67a8c48604adf7bdf6d6e9ea0
VsevolodX <vsevolodbiryukov@icloud.com>
2025-07-29 19:31:27 -0700

    update: set deps to match JL 5

--- scripts/rebuild.sh
+++ scripts/rebuild.sh
@@ -4,5 +4,5 @@ virtualenv .venv-3.10.12
 source .venv-3.10.12/bin/activate
 # These versions used in JL and API-examples configs
 pip install jupyterlab~=4.0.6 jupyterlite-core~=0.1.3
-jlpm add @jupyterlab/application@^4.0.0 @jupyterlab/notebook@^4.0.0 @mat3ra/esse
+jlpm add @jupyterlab/application@~4.0.6 @jupyterlab/notebook@~4.0.6 @mat3ra/esse
 jlpm run build
